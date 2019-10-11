e722aa05e711b0014c83460a9e26469c82d19f6e
MaTeng <1749156618@qq.com>
2019-10-11 20:39:39 +0800

    按需加载vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import {
+  Button,
+  Cell,
+  CellGroup
+} from 'vant'
+
+Vue
+  .use(Button)
+  .use(Cell)
+  .use(CellGroup)
+
 Vue.config.productionTip = false
 
 new Vue({
