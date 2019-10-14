b60096b7eaf106976bac3da9723a13d26979d40f
MaTeng <1749156618@qq.com>
2019-10-14 19:12:43 +0800

    使用 VeeValidate 插件完成表单验证登录校验

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 表单验证
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN' // 加载验证插件的语言包
+import * as rules from 'vee-validate/dist/rules'
 
 import {
   Button,
@@ -12,6 +16,29 @@ import {
   Field,
   Toast
 } from 'vant'
+
+// 配置使用中文语言
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule], // add the rule
+    message: zhCN.messages[rule] // add its message
+  })
+}
+
+// 注册为全局组件
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+
+// 拓展自定义校验规则
+// extend('规则名称'，配置对象)
+extend('phone', {
+  // 校验方法 value 是需要校验的数据，返回一个布尔值，表示验证成功与否
+  validate: function (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  // 错误提示消息
+  message: '请输入有效的手机号码'
+})
 
 Vue
   .use(Button)
@@ -28,3 +55,14 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 扩展自定义校验规则
+// extend('规则名称', 配置对象)
+extend('phone', {
+  // 验证方法，value 是需要校验的数据，返回一个布尔值，表示验证成功与否
+  validate: function (value) {
+    return /^1\d{10}$/.test(value)
+  },
+  // 错误提示消息
+  message: '请输入有效的手机号码'
+})
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,22 +3,33 @@
       <!-- 导航栏 -->
       <van-nav-bar title="标题"/>
           <!-- 登录表单 -->
-          <van-cell-group>
-            <van-field
-            v-model="user.mobile"
-                required
-                clearable
-                label="手机号"
-                placeholder="请输入手机号"
-            />
-            <van-field
-            v-model="user.code"
-                type="password"
-                label="验证码"
-                placeholder="请输入验证码"
-                required
-            />
-          </van-cell-group>
+          <ValidationObserver ref="loginForm">
+            <van-cell-group>
+              <!-- 表单验证插件                   （规则）-->
+              <ValidationProvider name="手机号" rules="required|phone" v-slot="{ errors }">
+                  <van-field
+                  v-model="user.mobile"
+                      required
+                      clearable
+                      label="手机号"
+                      placeholder="请输入手机号"
+                      :error-message="errors[0]"
+                  />
+              </ValidationProvider>
+
+              <!-- 表单验证插件 -->
+              <ValidationProvider name="验证码" rules="required|max:6" v-slot="{ errors }">
+                  <van-field
+                  v-model="user.code"
+                      type="password"
+                      label="验证码"
+                      placeholder="请输入验证码"
+                      required
+                      :error-message="errors[0]"
+                  />
+              </ValidationProvider>
+            </van-cell-group>
+          </ValidationObserver>
 
           <!-- 登录按钮 -->
           <div class="btn-wrap">
@@ -43,7 +54,14 @@ export default {
   methods: {
 
     async onLogin () {
-      // 表单验证
+      // 表单验证（表单验证插件js验证）
+      const isValid = await this.$refs.loginForm.validate()
+      // 如果验证失败，阻止表单提交
+      if (!isValid) {
+        return
+      }
+
+      // 验证通过调用loading接口
       const toast = this.$toast.loading({
         duration: 0, // 持续展示 toast
         forbidClick: true, // 禁用背景点击
