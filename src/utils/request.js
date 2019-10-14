1cb2a9665a0cc45f8e7260b3e59a287cd08d146e
MaTeng <1749156618@qq.com>
2019-10-14 19:42:32 +0800

    请求拦截器统一注入token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+import store from '@/store'// 在非组件模块中使用token需直接加载=this.$store
 
 // axios.create 方法
 // 建议使用 create 方式，我们可以拥有
@@ -16,5 +17,22 @@ const request = axios.create({
 request.defaults.transformResponse = [function (data) {
   return data ? jsonBig.parse(data) : {} // 换了一个转化方法 使得 计算更精确 保证id不失真
 }]
-
 export default request
+
+// 请求拦截器
+request.interceptors.request.use(function (config) {
+  // 在发起请求请做一些业务处理
+  // config是要发送请求的配置信息
+  const user = store.state.user
+  if (user) {
+    // Authorization 是后端要求的名字，不能瞎写
+    // 数据值 "Bearer空格token" 也是后端要求的数据格式，不能瞎写
+    // 千万!千万!千万!注意，Bearer 和 token 之间的空格不能少
+    // 什么意义？这是后端要求的，我们决定不了。
+    config.headers['Authorization'] = `Bearer ${user.token}` // 统一注入token 到headers属性 因为所有接口要求的token格式是一样的
+  }
+  return config
+}, function (error) {
+  // 对请求失败做处理
+  return Promise.reject(error)
+})
